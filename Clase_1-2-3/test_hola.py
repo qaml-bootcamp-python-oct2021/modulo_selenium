fa42e74e37fa0bebc1471ab07ef296dbf92373a1
Ivan de la fuente <ivan.delafuente@6connex.com>
2021-11-25 19:19:09 -0600

    update

--- Clase_1-2-3/test_hola.py
+++ Clase_1-2-3/test_hola.py
@@ -8,24 +8,23 @@ def setup():
     global driver
     browser = 'chrome'
     if browser == 'chrome':
-        driver_path = './modulo_selenium/driver/chromedriver'
+        driver_path = './drivers/chromedriver'
         driver = webdriver.Chrome(executable_path=driver_path)
     elif browser == 'firefox':
-        driver_path = './modulo_selenium/driver/geckodriver'
+        driver_path = './drivers/geckodriver'
         driver = webdriver.Firefox(executable_path=driver_path)
     else:
         raise RuntimeError('No existe')
-    return driver
 
 def test_open_qamind():
     url = 'https://qaminds.com/#/'
     driver.get(url)
-    time.sleep(3)
+    time.sleep(5)
 
 def test_open_youtbe():
     url = 'https://youtube.com'
     driver.get(url)
-    time.sleep(3)
+    time.sleep(5)
 
 def teardown():
     driver.quit()
