703e9e3ee590419d272054c4c432282d799c2fb0
Omar Garcia <omareduardogj@gmail.com>
2021-12-17 11:53:20 -0600

    Delete

--- clase_09/tests/test_init.py
+++
@@ -1,9 +0,0 @@
-from clase_09.drivers import factory_driver
-from clase_09.pages.home_page import HomePage
-import time
-
-def test_init():
-    driver = factory_driver.get_driver()
-    home_page = HomePage (driver)
-    time.sleep(3)
-    home_page.close_browser()
